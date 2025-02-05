e7029cd71e3300b24298c1bf8320ae92734d6d41
Semellet <58854141+Semellet@users.noreply.github.com>
2025-02-05 10:55:43 +0100

    Updated update.sh
    Added file/folder creation in home directory and moved the targeted log files to prevent permissions errors.

--- update.sh
+++ update.sh
@@ -2,7 +2,50 @@
 # A basic one stop shop to update Linux based systems. Utilising search, standard input/output filtering, and functions to seemlessly update the active distro.
 
 logfile=/var/log/updater.log
-errorlog=/var/log/updater_errors.log
+errorlog=/var/log/updater_errors.log#!/bin/bash
+# A basic one stop shop to update Linux based systems. Utilising search, standard input/output filtering, and functions to seemlessly update the active distro.
+
+logfile=~/bash-scripts/log/updater.log
+errorlog=~/bash-scripts/log/updater_errors.log
+
+check_exit_status() {
+	if [ $? -ne 0 ]
+	then 
+		echo "An error has occurred, please check the $errorlog file."
+	else
+		echo "The system is now up to date... it may need to be restarted."
+	fi
+}
+
+if [ ! -d ~/bash-scripts/log/ ]
+then
+	sudo mkdir ~/bash-scripts/log
+	sudo touch $logfile $errorlog
+	sudo chmod a+w $logfile $errorlog
+fi
+
+if [ -d /etc/pacman.d ]
+then 
+	# The host is based on Arch and runs the pacman update.
+	sudo pacman -Syu 1>$logfile 2>$errorlog
+	check_exit_status
+
+elif [ -d /etc/dnf ]
+then
+	# The host is running a Fedora/Redhat based system and runs the dnf update.
+	sudo dnf upgrade -y 1>$logfile 2>$errorlog
+	check_exit_status
+
+elif [ -d /etc/apt ]
+then
+	# The host is running a Debian based system and runs apt updates.
+	sudo apt-get update 1>$logfile 2>$errorlog && sudo apt-get dist-upgrade -y 1>>$logfile 2>>$errorlog
+	check_exit_status
+
+else
+	# The host is not running a standard package manager, so the script needs to be updated to reflect this.
+	echo "I cannot identify how to update this system.  Please check the documentation."
+fi
 
 check_exit_status() {
 	if [ $? -ne 0 ]
